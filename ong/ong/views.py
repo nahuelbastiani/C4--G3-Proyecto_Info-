c2e1d1eb9f164dd54ed96cb01fa80af8eb0083b9
AleProsDev <alejandro.f.pros@gmail.com>
2022-08-24 20:46:36 -0300

    -cambios en la pagina
    -actualizaciones y pruebas.

--- ong/ong/views.py
+++ ong/ong/views.py
@@ -1,5 +1,15 @@
+from re import template
 from django.shortcuts import render
+
+from eventos.models import Eventos
 
 def inicio(request):
 	template_name = 'inicio.html'
-	return render (request, template_name, {})
+
+	eventos = Eventos.objects.all()
+	print(eventos)
+
+	ctx = {
+		'eventos': eventos,
+	}
+	return render (request, template_name, ctx)
